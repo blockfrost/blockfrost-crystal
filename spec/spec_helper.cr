7c2cf43203a9b832b7e6d6d27a992f4524230ca3
Wout <hi@wout.codes>
2022-06-02 12:24:08 +0200

    test: prepare test suite

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,26 @@
 require "spec"
+require "webmock"
 require "../src/blockfrost"
+
+def fake_api_key
+  "mainnetVas4TOfOvQeVjTVGxxYNRLOt6Fb4FAKE"
+end
+
+def configure_api_key(api_key : String? = fake_api_key) : Void
+  Blockfrost.configure do |config|
+    config.api_key = api_key
+  end
+end
+
+def read_fixture(file : String) : String
+  path = "#{__DIR__}/fixtures/#{file}"
+  if File.exists?(path)
+    File.read(path)
+  else
+    raise Exception.new("Fixture #{file} does not exist.")
+  end
+end
+
+Spec.after_each do
+  WebMock.reset
+end
