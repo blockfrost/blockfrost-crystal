59a445e4a001320f6cc7b26770719db19a83a4b9
Wout <hi@wout.codes>
2022-10-13 15:53:53 +0200

    refactor: remove utils for now

--- src/blockfrost/utils.cr
+++
@@ -1,9 +0,0 @@
-module Blockfrost::Utils
-  extend self
-
-  def sanitize_query(
-    query : Hash(String, Int | String | Nil)
-  ) : Client::RequestData
-    query.reject { |_, v| v.nil? }.transform_values(&.to_s)
-  end
-end
