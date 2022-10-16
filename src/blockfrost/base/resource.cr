082f005c3664e23f36d95a748eaac5b73aa12690
Wout <hi@wout.codes>
2022-10-16 20:12:38 +0200

    feat: add macro for scoped resources with ordering and pagination

--- src/blockfrost/base/resource.cr
+++ src/blockfrost/base/resource.cr
@@ -115,4 +115,57 @@ abstract struct Blockfrost::BaseResource
       end
     {% end %}
   end
+
+  macro gets_all_scoped_with_order_and_pagination(
+    method_name,
+    return_type,
+    resource_path,
+    argument_type_declaration,
+    scope_argument_type_declaration
+  )
+    def self.{{method_name.id}}(
+      {{argument_type_declaration}},
+      {{scope_argument_type_declaration}},
+      order : QueryOrder? = nil,
+      count : QueryCount? = nil,
+      page : QueryPage? = nil
+    ) : {{return_type}}
+      {{return_type}}.from_json(
+        client.get({{resource_path}}, {
+          "order" => order.try(&.to_s),
+          "count" => count,
+          "page"  => page,
+        })
+      )
+    end
+
+    def self.{{method_name.id}}(
+      {{argument_type_declaration}},
+      {{scope_argument_type_declaration}},
+      order : String,
+      count : QueryCount? = nil,
+      page : QueryPage? = nil
+    ) : {{return_type}}
+      {{method_name.id}}(
+        {{argument_type_declaration.var}},
+        {{scope_argument_type_declaration.var}},
+        order_from_string(order),
+        count,
+        page
+      )
+    end
+
+    {% unless argument_type_declaration.nil? %}
+      def {{method_name.id}}(
+        {{scope_argument_type_declaration.var}},
+        **args
+      ) : {{return_type}}
+        self.class.{{method_name.id}}(
+          {{argument_type_declaration.var}},
+          {{scope_argument_type_declaration.var}},
+          **args
+        )
+      end
+    {% end %}
+  end
 end
