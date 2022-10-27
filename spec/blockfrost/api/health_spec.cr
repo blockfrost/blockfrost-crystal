ea6b308221fcf0eb7915bf774c3412be09881e6b
Wout <hi@wout.codes>
2022-10-27 19:15:21 +0200

    feat: add convenience methods to the health endpoints

--- spec/blockfrost/api/health_spec.cr
+++ spec/blockfrost/api/health_spec.cr
@@ -14,13 +14,33 @@ describe Blockfrost::Health do
     end
   end
 
+  describe ".is_healthy" do
+    it "fetches the actual health status" do
+      WebMock.stub(:get, "https://cardano-testnet.blockfrost.io/api/v0/health")
+        .to_return(body: read_fixture("health/get.200.json"))
+
+      Blockfrost::Health.is_healthy.should be_truthy
+    end
+  end
+
   describe ".clock" do
-    it "fetches the health server time endpoint" do
+    it "fetches the health clock endpoint" do
       WebMock.stub(:get,
         "https://cardano-testnet.blockfrost.io/api/v0/health/clock")
         .to_return(body: read_fixture("health/clock.200.json"))
 
       Blockfrost::Health.clock.server_time
+        .should eq(Time.unix_ms(1603400958947))
+    end
+  end
+
+  describe ".server_time" do
+    it "returns the value of the clock endpoint" do
+      WebMock.stub(:get,
+        "https://cardano-testnet.blockfrost.io/api/v0/health/clock")
+        .to_return(body: read_fixture("health/clock.200.json"))
+
+      Blockfrost::Health.server_time
         .should eq(Time.unix_ms(1603400958947))
     end
   end
--- src/blockfrost/api/health.cr
+++ src/blockfrost/api/health.cr
@@ -7,8 +7,16 @@ struct Blockfrost::Health
     Health.from_json(Client.get("health"))
   end
 
+  def self.is_healthy
+    get.is_healthy
+  end
+
   def self.clock
     Clock.from_json(Client.get("health/clock"))
+  end
+
+  def self.server_time
+    clock.server_time
   end
 
   struct Clock
