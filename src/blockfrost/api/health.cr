b9466909f37888703a850a2103d266502322667e
Wout <hi@wout.codes>
2022-10-29 16:36:22 +0200

    docs: add inline documentation for health and root endpoints

--- src/blockfrost/api/health.cr
+++ src/blockfrost/api/health.cr
@@ -3,18 +3,44 @@ struct Blockfrost::Health
 
   getter is_healthy : Bool
 
+  # Return backend status as a boolean. Your application should handle
+  # situations when backend for the given chain is unavailable.
+  #
+  # ```
+  # Blockfrost::Health.get.is_healthy
+  # # => true
+  # ```
   def self.get
     Health.from_json(Client.get("health"))
   end
 
+  # Alias for `.get.is_healthy`.
+  #
+  # ```
+  # Blockfrost::Health.is_healthy?
+  # # => true
+  # ```
   def self.is_healthy?
     get.is_healthy
   end
 
+  # This endpoint provides the current UNIX time. Your application might use
+  # this to verify if the client clock is not out of sync.
+  #
+  # ```
+  # Blockfrost::Health.clock.server_time
+  # # => 2020-10-22 21:09:18.947000000 UTC
+  # ```
   def self.clock
     Clock.from_json(Client.get("health/clock"))
   end
 
+  # Alias for `.clock.server_time`.
+  #
+  # ```
+  # Blockfrost::Health.server_time
+  # # => 2020-10-22 21:09:18.947000000 UTC
+  # ```
   def self.server_time
     clock.server_time
   end
--- src/blockfrost/api/root.cr
+++ src/blockfrost/api/root.cr
@@ -4,6 +4,12 @@ struct Blockfrost::Root
   getter url : String
   getter version : String
 
+  # Root endpoint has no other function than to point end users to
+  # documentation.
+  #
+  # ```
+  # Blockfrost::Root.get
+  # ```
   def self.get
     Root.from_json(Client.get("/"))
   end
