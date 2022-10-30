68b09bd6675b56ee395bf4d6d3e0a9a6d75b1f98
Wout <hi@wout.codes>
2022-10-30 08:55:31 +0100

    feat: add ipfs pin endpoint

--- spec/blockfrost/ipfs_spec.cr
+++ spec/blockfrost/ipfs_spec.cr
@@ -22,11 +22,32 @@ describe Blockfrost::IPFS do
   describe ".gateway" do
     it "retrieves an object from the IPFS gateway (useful if you do not want to rely on a public gateway" do
       WebMock.stub(:get,
-        "https://ipfs.blockfrost.io/api/v0/ipfs/gateway/something")
+        "https://ipfs.blockfrost.io/api/v0/ipfs/gateway/#{test_ipfs_path}")
         .to_return(body_io: read_fixture("ipfs/gateway.200.json"))
 
-      Blockfrost::IPFS.gateway("something")
+      Blockfrost::IPFS.gateway(test_ipfs_path)
         .should eq("ipfs.blockfrost.dev")
     end
   end
+
+  describe ".pin" do
+    it "pins an object" do
+      WebMock.stub(:post,
+        "https://ipfs.blockfrost.io/api/v0/ipfs/pin/add/#{test_ipfs_path}")
+        .with(body: "", headers: {
+          "Accept"       => "application/json",
+          "Content-Type" => "application/json",
+          "project_id"   => test_ipfs_api_key,
+        })
+        .to_return(body_io: read_fixture("ipfs/pin.200.json"))
+
+      pinned = Blockfrost::IPFS.pin(test_ipfs_path)
+      pinned.ipfs_hash.should eq(test_ipfs_path)
+      pinned.state.should eq(Blockfrost::IPFS::Pin::State::Queued)
+    end
+  end
+end
+
+private def test_ipfs_path
+  "QmPojRfAXYAXV92Dof7gtSgaVuxEk64xx9CKvprqu9VwA8"
 end
--- src/blockfrost/ipfs.cr
+++ src/blockfrost/ipfs.cr
@@ -1,11 +1,4 @@
-struct Blockfrost::IPFS
-  include JSON::Serializable
-
-  getter name : String
-  getter ipfs_hash : String
-  @[JSON::Field(converter: Blockfrost::Int64FromString)]
-  getter size : Int64
-
+module Blockfrost::IPFS
   def self.add(file_path : String)
     reader, writer = IO.pipe
 
@@ -17,7 +10,7 @@ struct Blockfrost::IPFS
 
     writer.close
 
-    from_json(
+    Object.from_json(
       Client.post(
         "ipfs/add",
         reader,
@@ -30,12 +23,31 @@ struct Blockfrost::IPFS
     String.from_json(Client.get("ipfs/gateway/#{ipfs_path}"))
   end
 
-  struct Pin
+  def self.pin(ipfs_path : String)
+    Pin.from_json(Client.post("ipfs/pin/add/#{ipfs_path}", ""))
+  end
+
+  struct Object
     include JSON::Serializable
 
     getter name : String
     getter ipfs_hash : String
     @[JSON::Field(converter: Blockfrost::Int64FromString)]
     getter size : Int64
+  end
+
+  struct Pin
+    include JSON::Serializable
+
+    Blockfrost.enum_castable_from_string(State, {
+      Queued,
+      Pinned,
+      Unpinned,
+      Failed,
+      Gc,
+    })
+
+    getter ipfs_hash : String
+    getter state : State
   end
 end
